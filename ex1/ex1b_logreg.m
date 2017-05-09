b1f37369f790c4779c42965be44e1b3419f7c3da
Anirudh Swaminathan <aniswami97@gmail.com>
2017-05-10 00:08:38 +0530

    [COMPLETE] Exercise 1b, which is logistic regression

--- ex1/ex1b_logreg.m
+++ ex1/ex1b_logreg.m
@@ -12,7 +12,7 @@ binary_digits = true;
 [train,test] = ex1_load_mnist(binary_digits);
 
 % Add row of 1s to the dataset to act as an intercept term.
-train.X = [ones(1,size(train.X,2)); train.X]; 
+train.X = [ones(1,size(train.X,2)); train.X];
 test.X = [ones(1,size(test.X,2)); test.X];
 
 % Training set dimensions
@@ -20,7 +20,7 @@ m=size(train.X,2);
 n=size(train.X,1);
 
 % Train logistic regression classifier using minFunc
-options = struct('MaxIter', 100);
+options = struct('MaxIter', 100, 'useMex', 0);
 
 % First, we initialize theta to some small random values.
 theta = rand(n,1)*0.001;
@@ -29,9 +29,9 @@ theta = rand(n,1)*0.001;
 %
 % TODO:  Implement batch logistic regression in the logistic_regression.m file!
 %
-tic;
-theta=minFunc(@logistic_regression, theta, options, train.X, train.y);
-fprintf('Optimization took %f seconds.\n', toc);
+%tic;
+%theta=minFunc(@logistic_regression, theta, options, train.X, train.y);
+%fprintf('Optimization took %f seconds.\n', toc);
 
 % Now, call minFunc again with logistic_regression_vec.m as objective.
 %
@@ -40,10 +40,10 @@ fprintf('Optimization took %f seconds.\n', toc);
 % time for your logistic_regression.m and logistic_regression_vec.m implementations.
 %
 % Uncomment the lines below to run your vectorized code.
-%theta = rand(n,1)*0.001;
-%tic;
-%theta=minFunc(@logistic_regression_vec, theta, options, train.X, train.y);
-%fprintf('Optimization took %f seconds.\n', toc);
+theta = rand(n,1)*0.001;
+tic;
+theta=minFunc(@logistic_regression_vec, theta, options, train.X, train.y);
+fprintf('Optimization took %f seconds.\n', toc);
 
 % Print out training accuracy.
 tic;
@@ -53,4 +53,3 @@ fprintf('Training accuracy: %2.1f%%\n', 100*accuracy);
 % Print out accuracy on the test set.
 accuracy = binary_classifier_accuracy(theta,test.X,test.y);
 fprintf('Test accuracy: %2.1f%%\n', 100*accuracy);
-
--- ex1/logistic_regression.m
+++ ex1/logistic_regression.m
@@ -2,16 +2,18 @@ function [f,g] = logistic_regression(theta, X,y)
   %
   % Arguments:
   %   theta - A column vector containing the parameter values to optimize.
-  %   X - The examples stored in a matrix.  
+  %   X - The examples stored in a matrix.
   %       X(i,j) is the i'th coordinate of the j'th example.
   %   y - The label for each example.  y(j) is the j'th example's label.
   %
 
   m=size(X,2);
-  
+  n = size(X,1);
+
   % initialize objective value and gradient.
   f = 0;
   g = zeros(size(theta));
+  h_x = zeros(size(y));
 
 
   %
@@ -21,4 +23,18 @@ function [f,g] = logistic_regression(theta, X,y)
   % TODO:  Compute the gradient of the objective by looping over the dataset and summing
   %        up the gradients (df/dtheta) for each example. Store the result in 'g'.
   %
+    for i=1:m
+        t_xi = 0;
+        for j=1:n
+            t_xi = t_xi + theta(j)*X(j,i);
+        end
+        h_x(i) = sigmoid(t_xi);
+        f = f - ((y(i)*log(h_x(i))) + ((1-y(i))*log(1-h_x(i))));
+    end
+%
+    for j=1:n
+        for i=1:m
+            g(j) = g(j) + (h_x(i) - y(i))*X(j, i);
+        end
+    end
 %%% YOUR CODE HERE %%%
